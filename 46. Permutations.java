fa712deef3d9cc0dcd87cefcc026782b6a78e3cd
kamal-bhatti <149489775+kamal-bhatti@users.noreply.github.com>
2026-09-24 16:52:52 +0530

    Add files via upload

---
+++ 46. Permutations.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(0, nums, result);
+        return result;
+    }
+    private void backtrack(int index, int[] nums, List<List<Integer>> result) {
+
+        if (index == nums.length) {
+            List<Integer> current = new ArrayList<>();
+            for (int num : nums) {
+                current.add(num);
+            }
+            result.add(current);
+            return;
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            swap(nums, index, i);
+            backtrack(index + 1, nums, result);
+            swap(nums, index, i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
